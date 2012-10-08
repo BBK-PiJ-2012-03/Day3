e84f47a777c0c4eff6fa8372b9e514aec9be2e4b
Alex <a.rokotas@gmail.com>
2012-10-08 16:30:28 +0100

    Done (second step)

--- ex4.groovy
+++ ex4.groovy
@@ -1,8 +1,8 @@
-println ("Give me tour text")
+println ("Give me your text")
 String s = System.console().readLine()
 int length = s.length()
 
 for (int i=0 ; i < length ; i++) {
 char c = s.charAt(i)
-println (c)
+print (" " + c)
 }
