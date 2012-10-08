699912758a3be67d7a400f1ab96a8e7b381681e7
Alex <a.rokotas@gmail.com>
2012-10-08 16:39:18 +0100

    DONE

--- ex5.groovy
+++ ex5.groovy
@@ -1,11 +1,14 @@
 println ("Give me your text")
 String s = System.console().readLine()
 int length = s.length()
-int counter
+int counter = 0
+
+println ("Give me your letter to search")
+String s2 = System.console().readLine()
 
 for (int i=0 ; i < length ; i++) {
 char c = s.charAt(i)
-if (c== "e")
+if (c== s2)
 counter++
 }
 
