be4b9119238b021f27a72a16becf0c56378f7d4b
Alex Rokotas <a.rokotas@gmail.com>
2012-10-08 22:54:09 +0100

    DONE

--- ex7.groovy
+++ ex7.groovy
@@ -5,28 +5,48 @@ int counter = 0
 boolean finished = false
 
 String memory = ""
+
+
+
 while (!finished) {
 
-for (int i=0 ; i < length ; i++) {
-
-  
- 
  println ("Give me your letter to search")
  String s2 = System.console().readLine()
 
 
+
+for (int i=0 ; i < length ; i++) {
+
+ for (int j=0 ; j < memory.length() ; j++)
+ {	
+	if (s2 == memory.charAt(j))
+	finished = true 
+ }
+
+ if (finished)
+	break;
+
+ 
+	
  char c = s.charAt(i)
- memory += s2
+ 
  if (c == s2)
  counter++
  
-
-   if ("s2"=="memory")
-   finished = true
  }
  
- println (counter)
-}
+ if (finished)
+ {
+     println ("Repeated character. Exiting the program...\nThank you for your cooperation. Good bye!")
+     break;
+ }
 
+
+ println ("There are " + counter + " in your text")
+ counter = 0
+ memory += s2
+ 
+ 
+}
 
 
